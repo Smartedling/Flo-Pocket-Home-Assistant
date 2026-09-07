8e29557e50cc466c53e6835be3c4eab24d7e9936
Smartedling <floscher86@gmail.com>
2026-09-07 08:54:56 +0200

    Add Flo Pocket Home Assistant integration

---
+++ custom_components/flo_pocket/const.py
@@ -0,0 +1,13 @@
+"""Constants for Flo Pocket."""
+
+DOMAIN = "flo_pocket"
+CONF_URL = "url"
+CONF_TOKEN = "token"
+DEFAULT_URL = "https://flo-pocket-api.vercel.app"
+
+CATEGORY_NAMES = {
+    "TODO": "Aufgaben",
+    "EINKAUF": "Einkauf",
+    "IDEE": "Ideen",
+    "NOTIZ": "Notizen",
+}
